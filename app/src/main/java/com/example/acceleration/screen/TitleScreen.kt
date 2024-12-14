b80aa891f68a51b009bd1adcfa8e325f8d212523
1023170 <b1023170@fun.ac.jp>
2024-12-14 16:14:22 +0900

    titleの画像表示

--- app/src/main/java/com/example/acceleration/screen/TitleScreen.kt
+++ app/src/main/java/com/example/acceleration/screen/TitleScreen.kt
@@ -4,15 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.acceleration.R
 import kotlinx.coroutines.delay
 
 class TitleScreen : AppCompatActivity() {
@@ -43,7 +48,11 @@ class TitleScreen : AppCompatActivity() {
                 finish()
             }
 
-            Text("🥺", fontSize = 128.sp) // 仮のロゴ
+            Image(
+                painter = painterResource(id = R.drawable.title), // your_imageを画像リソース名に置き換える
+                contentDescription = "Logo", // アクセシビリティ用説明文
+                modifier = Modifier.fillMaxSize()
+            )
         }
     }
 }
